e55095a4796550221b535ba3af62b2a6e2fa2af0
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-02 01:17:41 -0300

    Arquitetando pastas em DDD na API_Boletim

---
+++ DDD/API_Boletim/API_Boletim/API_Boletim/Interfaces/IAluno.cs
@@ -0,0 +1,19 @@
+﻿using API_Boletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        //CRUD
+        Aluno Create(Aluno a);
+        Aluno Delete(Aluno a);
+        Aluno SearchForId(int id);
+        List<Aluno> ReadAll();
+        Aluno Update(Aluno a);
+
+    }
+}
---
+++ DDD/API_Boletim/API_Boletim/API_Boletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Create(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Delete(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno SearchForId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Update(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
